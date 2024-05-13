3ac038412e5a5c07df4e7c12eb6628cc9db67a56
Homer Moncayo <homermoncallo@gmail.com>
2024-05-13 15:00:37 -0400

    Triying to fix login form on movile devices

--- src/components/ui/Card.tsx
+++ src/components/ui/Card.tsx
@@ -1,6 +1,6 @@
 export default function Card({ children }: {children: JSX.Element}) {
   return (
-    <div className="card w-4/12 bg-base-200 shadow-xl">
+    <div className="card w-100 md:w-6/12 mx-auto bg-base-200 shadow-xl">
       <div className="card-body">
         { children }
       </div>
--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -50,7 +50,8 @@ export default function Login() {
     <div className="flex flex-col min-h-screen">
       <Navbar />
       <div className="flex-1 flex justify-center items-center">
-        {
+        <div className="container">
+          {
           (!auth && !loading) && (
             <Card>
               <form onSubmit={handleLogin}>
@@ -96,6 +97,7 @@ export default function Login() {
             </Card>
           )
         }
+        </div>
       </div>
       <Footer />
     </div>
